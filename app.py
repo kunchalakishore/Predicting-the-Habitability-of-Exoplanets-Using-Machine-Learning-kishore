05245803500f765b0f3887a959f97de09f1cf014
khwahish <khwahishseth@gmail.com>
2026-01-13 18:22:55 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load trained ML model
+model = pickle.load(open("habitability_model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "NASA Exoplanet Habitability Prediction API is running 🚀"
+    })
+
+@app.route("/ui")
+def ui():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json(force=True)
+
+        # 4 features from UI
+        planet_temp_z = float(data["planet_temp_z"])
+        planet_size_z = float(data["planet_size_z"])
+        star_temp_z = float(data["star_temp_z"])
+        star_energy_z = float(data["star_energy_z"])
+
+        # 3 missing features (DEFAULT = 0.0 → mean of z-score)
+        star_gravity_z = 0.0
+        star_size_z = 0.0
+        brightness_z = 0.0
+
+        # TOTAL = 7 FEATURES (MATCH MODEL TRAINING)
+        X = np.array([
+            planet_temp_z,
+            planet_size_z,
+            star_temp_z,
+            star_energy_z,
+            star_gravity_z,
+            star_size_z,
+            brightness_z
+        ]).reshape(1, -1)
+
+        score = model.predict_proba(X)[0][1]
+        prediction = "Habitable" if score >= 0.5 else "Non-Habitable"
+
+        return jsonify({
+            "habitability_score": round(float(score), 3),
+            "prediction": prediction
+        })
+
+    except Exception as e:
+        print("Prediction error:", e)
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
